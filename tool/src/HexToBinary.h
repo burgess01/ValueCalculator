9c743414aa3833a48097a073bbaf71d7f8f9e9e8
Ginterfrankovitchm <Ginterfrankovitchm@allegheny.edu>
2021-12-07 17:27:22 -0500

    I fixed most of hex to binary

--- tool/src/HexToBinary.h
+++ tool/src/HexToBinary.h
@@ -13,58 +13,66 @@
 int h2b(){
     int res = 0;
     char hex[LENGTH];
-    int binary[LENGTH * 4];
     printf("Enter a hexidecimal number to convert to binary:");
     scanf("%s", hex);
     // split hex into an array
-    int len = (int) strlen(hex);
+    int len = strlen(hex);
+    printf("The length is %d\n",len);
+    int decimal[len];
+    int binary[len*4];
     // iterate through, turning letter values to numbers
     for(int i = 0; i < len; i++){
         if(hex[i] == 'A'){
-            hex[i] = 10;
+            decimal[i] = 10;
         }
         else if(hex[i] == 'B'){
-            hex[i] = 11;
+            decimal[i] = 11;
         }
         else if(hex[i] == 'C'){
-            hex[i] = 12;
+            decimal[i] = 12;
         }
         else if(hex[i] == 'D'){
-            hex[i] = 13;
+            decimal[i] = 13;
         }
         else if(hex[i] == 'E'){
-            hex[i] = 14;
+            decimal[i] = 14;
         }
         else if(hex[i] == 'F'){
-            hex[i] = 15;
+            decimal[i] = 15;
         }
         else{
-            hex[i] = hex[i] - 48;
+            decimal[i] = decimal[i] - 48;
         }
     }
     for(int i = 0; i < len; i++){
-        printf("%d\n", hex[i]);
+        printf("Decimal values are %d\n", decimal[i]);
     }
     // convert from decimal to binary
     for(int i = 0; i < len; i++){
-        res = d2b(hex[i]);
+        res = d2b(decimal[i]);
+        printf("res is %d\n", res);
+        binary[i] = res;
         // add values one by one to binary[]
+        /*
         for(int i = 0; i < 4; i++)
             // if len res is less than 4:
             // loop the difference and add those to binary[]
-        binary[i] = res;
+            binary[i] = res;
+        */
     }
+    
     // return output
-    for(int i = 0; i < len; i++){
+    unsigned long result = 0;
+    int i = 0;
+    while(len>0){
+        printf("i is %d\n", i);
         printf("%d\n", binary[i]);
+        result = result + (pow(10,((len-1)*4))*binary[i]);
+        len = len-1;
+        i++;
     }
-    unsigned long result;
-    if(len == 1){
-        // combine into one variable
-        for(int i = 0; i < len; i++){
-            result = (result * 10) + binary[i];
-        }
-    }
+    
+    printf("The result is %lu\n", result);
     // print out list one by one on same line
     return 0;
 }
